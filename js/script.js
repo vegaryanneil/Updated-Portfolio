c156f982f66479cce2c0b9c837839c8948d58f5c
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-03-18 22:21:45 -0400

    Started the update to the portfolio, added some copy to main page

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+//   Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  var collapsibleElem = document.querySelector('.collapsible');
+  var collapsibleInstance = M.Collapsible.init(collapsibleElem);
+  
+// Floating Button
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+  });
