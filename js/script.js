84d59e79f085473475debb0d3a8abd14e155c1e6
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-03-28 09:42:12 -0400

    Utilizing GSAP and ScrollMagic for animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-const exploreButton = document.querySelector(".mountain-exp");
+let controller;
+let slideScene;
 
-window.addEventListener("scroll", scrollReveal);
-
-function scrollReveal() {
-  const hikePos = exploreButton.getBoundingClientRect().top;
-  console.log(hikePos);
-  if(hikePos < 0) {
-    exploreButton.style.color = "red"
-  }
-
-}
+function animateSlides(){
+  // Init Controller
+  controller = new ScrollMagic.Controller();
+  // Select Some things
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+  // Loop each slide
+  sliders.forEach(slide => {
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+    // GSAP
+    const sliderTimeline = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut"}
+    });
+    slideTl.fromTo(revealImg, { x: "0%"}, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2}, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%"}, { x: "100%" }, "-=0.75");
+    slideTl.fromTo(nav, { y:"-100%"}, { y: "0%" },"-=0.5");
+    
+  })}
