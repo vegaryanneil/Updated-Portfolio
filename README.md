e30cdb3a9b4349b22244c7a9d104003c8974d25b
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-03-30 02:22:25 -0400

    Added Call to action page, cleaned up HTML, CSS, JS, Added READMEdetails, Added Screenshot photos

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # Updated-Portfolio
 
-## Links
+![Landing Page](images/landingPageUpdate.png)
+## Description
+
+This is an update to my initial portfolio. This is update comes after diving into Javascript. I wanted to incorporate more interactions in my portfolio. I did this by attempting multiple animations, however, I succeeded in two major ones. I added a cursor animation as well as a burger menu animation.
+
+![Nav Page](images/burgerMenuUpdate.png)
+
+## To Do List
+1. Project Page
+2. About Page
+3. Service Page
+4. Clean Up Home Page
+5. Responsiveness
+
+## Outside Tools Used
+ScrollMagic - Failed
+
+GSAP - Mostly Successful
+## Access
 
 https://github.com/vegaryanneil/Updated-Portfolio
 
 https://vegaryanneil.github.io/Updated-Portfolio/
+
+## Resources
+
+Dev Ed
+
+https://www.youtube.com/watch?v=H4MkGzoACpQ
+
+https://www.youtube.com/watch?v=TpwpAYi-p2w
--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-// TO BE CONTINUED - DOES NOT WORK
-let controller;
-let slideScene;
+// // TO BE CONTINUED - DOES NOT WORK
+// let controller;
+// let slideScene;
 
-function animateSlides(){
-  // Init Controller
-  controller = new ScrollMagic.Controller();
-  // Select Some things
-  const sliders = document.querySelectorAll(".slide");
-  const nav = document.querySelector(".nav-header");
-  // Loop each slide
-  sliders.forEach(slide => {
-    const revealImg = slide.querySelector(".reveal-img");
-    const img = slide.querySelector("img");
-    const revealText = slide.querySelector(".reveal-text");
-    // GSAP
-    const sliderTimeline = gsap.timeline({
-      defaults: { duration: 1, ease: "power2.inOut"}
-    });
-    slideTl.fromTo(revealImg, { x: "0%"}, { x: "100%" });
-    slideTl.fromTo(img, { scale: 2}, { scale: 1 }, "-=1");
-    slideTl.fromTo(revealText, { x: "0%"}, { x: "100%" }, "-=0.75");
-    slideTl.fromTo(nav, { y:"-100%"}, { y: "0%" },"-=0.5");
+// function animateSlides(){
+//   // Init Controller
+//   controller = new ScrollMagic.Controller();
+//   // Select Some things
+//   const sliders = document.querySelectorAll(".slide");
+//   const nav = document.querySelector(".nav-header");
+//   // Loop each slide
+//   sliders.forEach(slide => {
+//     const revealImg = slide.querySelector(".reveal-img");
+//     const img = slide.querySelector("img");
+//     const revealText = slide.querySelector(".reveal-text");
+//     // GSAP
+//     const sliderTimeline = gsap.timeline({
+//       defaults: { duration: 1, ease: "power2.inOut"}
+//     });
+//     slideTl.fromTo(revealImg, { x: "0%"}, { x: "100%" });
+//     slideTl.fromTo(img, { scale: 2}, { scale: 1 }, "-=1");
+//     slideTl.fromTo(revealText, { x: "0%"}, { x: "100%" }, "-=0.75");
+//     slideTl.fromTo(nav, { y:"-100%"}, { y: "0%" },"-=0.5");
     
-  })}
-  // --------------------
+//   })}
+//   // --------------------
 // Navigation Animation
 const mouse = document.querySelector(".cursor");
 const mouseTxt = mouse.querySelector("span");
