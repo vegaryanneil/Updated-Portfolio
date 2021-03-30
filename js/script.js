5d6c2687af8ce9e98b2bcc366da87611a9c1d3fd
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-03-30 01:38:37 -0400

    Followed guide by DevEd to get animation on cursor and burger menu to work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// TO BE CONTINUED - DOES NOT WORK
 let controller;
 let slideScene;
 
@@ -22,3 +23,54 @@ function animateSlides(){
     slideTl.fromTo(nav, { y:"-100%"}, { y: "0%" },"-=0.5");
     
   })}
+  // --------------------
+// Navigation Animation
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+// Cursor Animation - when the cursor hovers over a specific button, a new class will be applied.
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+  if (item.classList.contains("button")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, { y: "0%" });
+    mouseTxt.innerText = "click";
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseTxt.innerText = "";
+    gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    // Creates the X in the burger menu
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "#232323" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "#232323" });
+    gsap.to("#logo", 1, { color: "#232323" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    // When you click on the X to class nav bar - it returns everything back to normal
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "#FF4E00" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "#FF4E00" });
+    gsap.to("#logo", 1, { color: "#232323" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+
+burger.addEventListener("click", navToggle);
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
